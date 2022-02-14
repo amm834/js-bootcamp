996a1312b5f27f870b06ec2672e227eefa8e9fb5
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-15 02:49:42 +0630

    Hiding tutorial text

--- 02_movie_fight/app.js
+++ 02_movie_fight/app.js
@@ -7,6 +7,7 @@ const autocompleteConfig = {
 		`;
 	},
 	onOptionSelect(movie) {
+		document.getElementById('tutorial').classList.add('d-none')
 		onMovieSelect(movie)
 	},
 	inputValue(movie) {
@@ -38,62 +39,62 @@ createAutoComplete({
 })
 
 
-// const onMovieSelect = async movie => {
-// 	const response = await axios.get('http://www.omdbapi.com/', {
-// 		params: {
-// 			apikey: "b24ef25a", i: movie.imdbID
-// 		}
-// 	})
-// 	console.log(response.data)
-// 	document.querySelector('#summary').innerHTML = movieTemplate(response.data)
-// }
-//
-// const movieTemplate = movieDetail => {
-// 	return `
-// 	   <div class="card my-3 border-0">
-//             <div class="row g-0">
-//                 <div class="col-md-4">
-//                     <img src="${movieDetail.Poster}" class="img-fluid rounded-start">
-//                 </div>
-//                 <div class="col-md-8">
-//                     <div class="card-body">
-//                         <h5 class="card-title">${movieDetail.Title}</h5>
-//                         <h6>${movieDetail.Genre}</h6>
-//                         <p class="card-text">${movieDetail.Plot}</p>
-//                     </div>
-//                 </div>
-//             </div>
-//         </div>
-//         <div class="card text-white bg-primary mb-3">
-//         	<div class="card-body">
-//         		<h5 class="card-title">${movieDetail.Awards}</h5>
-//         		<p class="card-text">Awards</p>
-//         	</div>
-//         </div>
-//         <div class="card text-white bg-primary mb-3">
-//         	<div class="card-body">
-//         		<h5 class="card-title">${movieDetail.imdbRating}</h5>
-//         		<p class="card-text">IMDB Rating</p>
-//         	</div>
-//         </div>
-//           <div class="card text-white bg-primary mb-3">
-//         	<div class="card-body">
-//         		<h5 class="card-title">${movieDetail.BoxOffice}</h5>
-//         		<p class="card-text">Box Office</p>
-//         	</div>
-//         </div>
-//         <div class="card text-white bg-primary mb-3">
-//         	<div class="card-body">
-//         		<h5 class="card-title">${movieDetail.Metascore}</h5>
-//         		<p class="card-text">Meta Score</p>
-//         	</div>
-//         </div>
-//         <div class="card text-white bg-primary mb-3">
-//         	<div class="card-body">
-//         		<h5 class="card-title">${movieDetail.imdbVotes}</h5>
-//         		<p class="card-text">IMDB Votes</p>
-//         	</div>
-//         </div>
-// 	`
-// }
+const onMovieSelect = async movie => {
+	const response = await axios.get('http://www.omdbapi.com/', {
+		params: {
+			apikey: "b24ef25a", i: movie.imdbID
+		}
+	})
+	console.log(response.data)
+	document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+}
+
+const movieTemplate = movieDetail => {
+	return `
+	   <div class="card my-3 border-0">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${movieDetail.Poster}" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${movieDetail.Title}</h5>
+                        <h6>${movieDetail.Genre}</h6>
+                        <p class="card-text">${movieDetail.Plot}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card text-white bg-primary mb-3">
+        	<div class="card-body">
+        		<h5 class="card-title">${movieDetail.Awards}</h5>
+        		<p class="card-text">Awards</p>
+        	</div>
+        </div>
+        <div class="card text-white bg-primary mb-3">
+        	<div class="card-body">
+        		<h5 class="card-title">${movieDetail.imdbRating}</h5>
+        		<p class="card-text">IMDB Rating</p>
+        	</div>
+        </div>
+          <div class="card text-white bg-primary mb-3">
+        	<div class="card-body">
+        		<h5 class="card-title">${movieDetail.BoxOffice}</h5>
+        		<p class="card-text">Box Office</p>
+        	</div>
+        </div>
+        <div class="card text-white bg-primary mb-3">
+        	<div class="card-body">
+        		<h5 class="card-title">${movieDetail.Metascore}</h5>
+        		<p class="card-text">Meta Score</p>
+        	</div>
+        </div>
+        <div class="card text-white bg-primary mb-3">
+        	<div class="card-body">
+        		<h5 class="card-title">${movieDetail.imdbVotes}</h5>
+        		<p class="card-text">IMDB Votes</p>
+        	</div>
+        </div>
+	`
+}
 
