ca6ac016028b2f5bfc6037c59e933b93a7b31a5b
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-15 01:01:37 +0630

    Expand statistics

--- 02_movie_fight/app.js
+++ 02_movie_fight/app.js
@@ -10,8 +10,7 @@ const results = document.querySelector('.results')
 const fetchData = async (searchTerm) => {
 	const response = await axios.get(url, {
 		params: {
-			apikey: "b24ef25a",
-			s: searchTerm
+			apikey: "b24ef25a", s: searchTerm
 		}
 	})
 
@@ -68,8 +67,7 @@ input.addEventListener('input', debounce(onInput))
 const onMovieSelect = async movie => {
 	const response = await axios.get(url, {
 		params: {
-			apikey: "b24ef25a",
-			i: movie.imdbID
+			apikey: "b24ef25a", i: movie.imdbID
 		}
 	})
 	console.log(response.data)
@@ -92,5 +90,36 @@ const movieTemplate = movieDetail => {
                 </div>
             </div>
         </div>
+        <div class="card text-white bg-primary mb-3">
+        	<div class="card-body">
+        		<h5 class="card-title">${movieDetail.Awards}</h5>
+        		<p class="card-text">Awards</p>
+        	</div>
+        </div>   
+        <div class="card text-white bg-primary mb-3">
+        	<div class="card-body">
+        		<h5 class="card-title">${movieDetail.imdbRating}</h5>
+        		<p class="card-text">IMDB Rating</p>
+        	</div>
+        </div>
+          <div class="card text-white bg-primary mb-3">
+        	<div class="card-body">
+        		<h5 class="card-title">${movieDetail.BoxOffice}</h5>
+        		<p class="card-text">Box Office</p>
+        	</div>
+        </div>
+        <div class="card text-white bg-primary mb-3">
+        	<div class="card-body">
+        		<h5 class="card-title">${movieDetail.Metascore}</h5>
+        		<p class="card-text">Meta Score</p>
+        	</div>
+        </div>      
+        <div class="card text-white bg-primary mb-3">
+        	<div class="card-body">
+        		<h5 class="card-title">${movieDetail.imdbVotes}</h5>
+        		<p class="card-text">IMDB Votes</p>
+        	</div>
+        </div>    
 	`
 }
+
