e8411a12a14dcaa8d3a89793283116dce841984e
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-15 00:47:49 +0630

    Making movie summary template and showing in the browser

--- 02_movie_fight/app.js
+++ 02_movie_fight/app.js
@@ -73,4 +73,24 @@ const onMovieSelect = async movie => {
 		}
 	})
 	console.log(response.data)
+	document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+}
+
+const movieTemplate = movieDetail => {
+	return `
+	   <div class="card my-3 border-0">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <img src="${movieDetail.Poster}" class="img-fluid rounded-start">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${movieDetail.Title}</h5>
+                        <h6>${movieDetail.Genre}</h6>
+                        <p class="card-text">${movieDetail.Plot}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+	`
 }
