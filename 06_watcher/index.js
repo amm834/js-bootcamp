af588364a7543034f9af6c7c80814c739e5c3219
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-19 15:20:55 +0630

    Adding debounce

--- 06_watcher/index.js
+++ 06_watcher/index.js
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 const chokidar = require('chokidar')
+const debounce = require('lodash.debounce')
 
+const start = () => {
+	console.log('Starting program')
+}
 chokidar.watch('.')
-	.on('add', () => console.log('file added'))
-	.on('change', () => console.log('file change'))
-	.on('unlink', () => console.log('file unlinked'))
+	.on('add', debounce(start, 100))
+		.on('change', () => console.log('file change'))
+		.on('unlink', () => console.log('file unlinked'))
