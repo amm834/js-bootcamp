92eae8bc572647e6bb1bf6baea06eaa032e3e75e
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-19 14:01:50 +0630

    Multiple implementation between promises

--- 05_list/index.js
+++ 05_list/index.js
@@ -1,25 +1,23 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
+const util = require('util')
+
+// Method #2
+// const lstat = util.promisify(fs.lstat)
+
+const {lstat} = fs.promises
 
 fs.readdir(process.cwd(), (err, filenames) => {
 	if (err) throw new Error(err)
-	const allStats = Array(filenames.length).fill(null)
-
-	filenames.forEach(filename => {
-		fs.lstat(filename, (err, stats) => {
-			if (err) throw new Error(err);
-
-			const index = filenames.indexOf(filename)
-
-			allStats[index] = stats;
-
-			const ready = allStats.every(stats => stats)
-			if (ready) {
-				allStats.forEach((stats, index) => {
-					console.log(filenames[index], stats.isFile())
-				})
-			}
-		})
-	})
 })
+
+// Method #1
+// const lstat = (filename) => {
+// 	return new Promise((resolve, reject) => {
+// 		fs.lstat(filename, (err, stats) => {
+// 			if (err) reject(err);
+// 			resolve(lstat)
+// 		})
+// 	})
+// }
