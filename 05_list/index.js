af9f67a5fec9cefb47a5967e15634a7c8f26d764
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-19 14:18:47 +0630

    Good implementation done

--- 05_list/index.js
+++ 05_list/index.js
@@ -1,32 +1,17 @@
 #!/usr/bin/env node
 
 const fs = require('fs');
-const util = require('util')
 
-// Method #2
-// const lstat = util.promisify(fs.lstat)
 
 const {lstat} = fs.promises
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
 	if (err) throw new Error(err)
 
-	filenames.forEach(async filename => {
-		try {
-			const stats = await lstat(filename)
-			console.log(filename, stats.isFile())
-		} catch (errors) {
-			console.log(errors)
-		}
+	const statsPromises = filenames.map(filename => lstat(filename))
+	const allStats = await Promise.all(statsPromises)
+	allStats.forEach((stats, index) => {
+		console.log(filenames[index], stats.isFile())
 	})
 })
 
-// Method #1
-// const lstat = (filename) => {
-// 	return new Promise((resolve, reject) => {
-// 		fs.lstat(filename, (err, stats) => {
-// 			if (err) reject(err);
-// 			resolve(stats)
-// 		})
-// 	})
-// }
