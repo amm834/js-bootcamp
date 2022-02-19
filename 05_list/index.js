2aeeff97cfb45055a0af417d26a4b35d7944609a
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-19 13:50:03 +0630

    Option #1 implementation

--- 05_list/index.js
+++ 05_list/index.js
@@ -4,12 +4,22 @@ const fs = require('fs');
 
 fs.readdir(process.cwd(), (err, filenames) => {
 	if (err) throw new Error(err)
+	const allStats = Array(filenames.length).fill(null)
 
-	filenames.forEach(path => {
-		fs.lstat(path, (err, stats) => {
-			if (err) throw new Error(err)
+	filenames.forEach(filename => {
+		fs.lstat(filename, (err, stats) => {
+			if (err) throw new Error(err);
 
-			console.log(path, stats.isFile())
+			const index = filenames.indexOf(filename)
+
+			allStats[index] = stats;
+
+			const ready = allStats.every(stats => stats)
+			if (ready) {
+				allStats.forEach((stats, index) => {
+					console.log(filenames[index], stats.isFile())
+				})
+			}
 		})
 	})
 })
