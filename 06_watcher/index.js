ab9ab3ab3f3e476bce0269c5e3fc311a3b5048aa
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-19 15:35:54 +0630

    Handling cli tool with coporal

--- 06_watcher/index.js
+++ 06_watcher/index.js
@@ -1,11 +1,20 @@
 #!/usr/bin/env node
 const chokidar = require('chokidar')
 const debounce = require('lodash.debounce')
+const program = require('caporal')
 
 const start = () => {
 	console.log('Starting program')
 }
-chokidar.watch('.')
-	.on('add', debounce(start, 100))
-		.on('change', () => console.log('file change'))
-		.on('unlink', () => console.log('file unlinked'))
+
+program.version('1.0.0')
+	.argument("[filename]", "Filename to execute the program")
+	.action(args => {
+		console.log(args)
+	})
+program.parse(process.argv)
+
+// chokidar.watch('.')
+// 	.on('add', debounce(start, 100))
+// 	.on('change', () => console.log('file change'))
+// 	.on('unlink', () => console.log('file unlinked'))
