e383663a5210abc79d91a28057c4b9ae69129886
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-19 15:59:58 +0630

    Execute js code via child process

--- 06_watcher/index.js
+++ 06_watcher/index.js
@@ -3,25 +3,27 @@ const chokidar = require('chokidar')
 const debounce = require('lodash.debounce')
 const program = require('caporal')
 const fs = require('fs').promises;
-
-const start = () => {
-	console.log('Starting program')
-}
+const {spawn} = require('child_process')
 
 program.version('1.0.0')
 	.argument("[filename]", "Filename to execute the program")
 	.action(async ({filename}) => {
 		const name = filename ?? 'index.js';
+
 		try {
 			const file = await fs.access(name)
 		} catch (err) {
 			throw new Error(`${name} file not found.`)
 		}
 
+		const start = debounce(() => {
+			spawn('node', [name], {stdio: 'inherit'})
+		}, 100);
+
 		chokidar.watch('.')
-			.on('add', debounce(start, 100))
-			.on('change', () => console.log('file change'))
-			.on('unlink', () => console.log('file unlinked'))
+			.on('add', start)
+			.on('change', start)
+			.on('unlink', start)
 	})
 program.parse(process.argv)
 
