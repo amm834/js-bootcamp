2df842a1c57176853bb541051e8be8bd3248a41c
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-19 13:33:22 +0630

    Implementing bad code

--- 05_list/index.js
+++ 05_list/index.js
@@ -4,5 +4,12 @@ const fs = require('fs');
 
 fs.readdir(process.cwd(), (err, filenames) => {
 	if (err) throw new Error(err)
-	console.log(filenames)
+
+	filenames.forEach(path => {
+		fs.lstat(path, (err, stats) => {
+			if (err) throw new Error(err)
+
+			console.log(path, stats.isFile())
+		})
+	})
 })
