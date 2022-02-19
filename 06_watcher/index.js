73ef52343d1270f266c2f3fcc4559f2dca5e2ec2
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-19 15:45:38 +0630

    Ensuring file exists

--- 06_watcher/index.js
+++ 06_watcher/index.js
@@ -2,6 +2,7 @@
 const chokidar = require('chokidar')
 const debounce = require('lodash.debounce')
 const program = require('caporal')
+const fs = require('fs').promises;
 
 const start = () => {
 	console.log('Starting program')
@@ -9,12 +10,18 @@ const start = () => {
 
 program.version('1.0.0')
 	.argument("[filename]", "Filename to execute the program")
-	.action(args => {
-		console.log(args)
+	.action(async ({filename}) => {
+		const name = filename ?? 'index.js';
+		try {
+			const file = await fs.access(name)
+		} catch (err) {
+			throw new Error(`${name} file not found.`)
+		}
+
+		chokidar.watch('.')
+			.on('add', debounce(start, 100))
+			.on('change', () => console.log('file change'))
+			.on('unlink', () => console.log('file unlinked'))
 	})
 program.parse(process.argv)
 
-// chokidar.watch('.')
-// 	.on('add', debounce(start, 100))
-// 	.on('change', () => console.log('file change'))
-// 	.on('unlink', () => console.log('file unlinked'))
