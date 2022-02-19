c8a17285ea12b3e80127ba006b53186d17efdfc1
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-19 14:47:42 +0630

    List project done

--- 05_list/index.js
+++ 05_list/index.js
@@ -2,14 +2,18 @@
 
 const fs = require('fs');
 const chalk = require('chalk')
+const path = require('path')
 
-const {lstat} = fs.promises
+const {lstat} = fs.promises;
 
-fs.readdir(process.cwd(), async (err, filenames) => {
-	if (err) throw new Error(err)
+const targetDir = process.argv[2] ?? process.cwd();
 
-	const statsPromises = filenames.map(filename => lstat(filename))
+fs.readdir(targetDir, async (err, filenames) => {
+	if (err) throw new Error(err);
+
+	const statsPromises = filenames.map(filename => lstat(path.join(targetDir, filename)))
 	const allStats = await Promise.all(statsPromises)
+
 	allStats.forEach((stats, index) => {
 		if (stats.isFile()) {
 			console.log(chalk.magenta(filenames[index]))
