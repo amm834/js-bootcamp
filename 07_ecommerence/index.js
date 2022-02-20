c0c0b825facca5a55013c5e6f3eb403b7bdab057
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-20 10:33:33 +0630

    Parsing form data with hand

--- 07_ecommerence/index.js
+++ 07_ecommerence/index.js
@@ -3,14 +3,28 @@ const app = express()
 
 app.get('/', (req, res) => {
 	res.send(`
-    <form>
-    <input type="email" placeholder="email">
-    <input type="password" placeholder="password">
-    <input type="password" placeholder="confirm password">
+    <form method="post">
+    <input type="email" placeholder="email" name="email">
+    <input type="password" placeholder="password" name="password">
+    <input type="password" placeholder="confirm password" name="confirmPassword">
     <button type="submit">Sign Up</button>
 	</form>
     `)
 })
+
+app.post('/', (req, res) => {
+	req.on('data', data => {
+		const rawFormdata = data.toString()
+		const parsed = rawFormdata.split('&')
+		const formData = {}
+		for (const data of parsed) {
+			const [key, value] = data.split('=')
+			formData[key] = value;
+		}
+		console.log(formData)
+	})
+})
+
 
 app.listen(3000, () => {
 	console.log('Server is running at port 3000')
