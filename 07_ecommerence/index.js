6d7db8359c6bb64e259d7c493eba63a3ed48df6d
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-21 00:30:56 +0630

    Creating product routes

--- 07_ecommerence/index.js
+++ 07_ecommerence/index.js
@@ -2,14 +2,19 @@ const express = require('express')
 const bodyParser = require("body-parser");
 const userRepo = require('./repositories/users')
 const cookieSession = require('cookie-session')
+const authRoute = require('./routes/admin/auth')
+const proudctRoute = require('./routes/admin/product')
+
 const app = express()
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cookieSession({
 	keys: ['a secret key']
 }))
 
-const authRoute = require('./routes/admin/auth')
+// routes
 app.use(authRoute)
+app.use(proudctRoute)
 
 app.listen(3000, () => {
 	console.log('Server is running at port 3000')
