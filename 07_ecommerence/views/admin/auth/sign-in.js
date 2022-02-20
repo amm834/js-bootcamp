398ca0da90c865b6b4b7c54a8bfb6f70fbd30355
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-21 00:25:22 +0630

    Update styling

--- 07_ecommerence/views/admin/auth/sign-in.js
+++ 07_ecommerence/views/admin/auth/sign-in.js
@@ -3,12 +3,23 @@ const {getErrors} = require('../../helper')
 
 module.exports = ({errors}) => layout({
 	content: `
-	 <form method="post">
-    <input  placeholder="email" name="email">
-    ${getErrors(errors, 'email')}
-    <input placeholder="password" name="password">
-        ${getErrors(errors, 'password')}
-    <button type="submit">Sign In</button>
-	</form>
+	<section class="row">
+    <div class="col-md-8 offset-md-2 ">
+        <form method="post" class="card p-5">
+            <h1 class="text-center mb-5">Sign In</h1>
+            <div class="mb-3">
+                <input placeholder="email" name="email" class="form-control">
+                <div class="text-danger">    ${getErrors(errors, 'email')}
+                </div>
+            </div>
+            <div class="mb-3">
+                <input placeholder="password" name="password" class="form-control">
+                <div class="text-danger"> ${getErrors(errors, 'password')}</div>
+            </div>
+            <button class="btn btn-success" type="submit">Sign In</button>
+
+        </form>
+    </div>
+    </section>
 	`
 })
--- 07_ecommerence/views/admin/auth/sign-up.js
+++ 07_ecommerence/views/admin/auth/sign-up.js
@@ -4,16 +4,33 @@ const {getErrors} = require('../../helper')
 module.exports = ({errors}) => {
 	return layout({
 		content: `
-    <form method="post">
-    <input placeholder="email" name="email">
-    ${getErrors(errors, 'email')}
-    <input  placeholder="password" name="password">
-        ${getErrors(errors, 'password')}
-    <input placeholder="confirm password" name="confirmPassword">
-        ${getErrors(errors, 'confirmPassword')}
-
-    <button type="submit">Sign Up</button>
-	</form>
+  <section class="row">
+    <div class="col-md-8 offset-md-2">
+        <h6 class="text-center mb-5">Sign Up</h6>
+        <form method="post" class="card p-5">
+            <div class="mb-3">
+                <input placeholder="email" name="email" class="form-control">
+                <div class="text-danger">
+                    ${getErrors(errors, 'email')}
+                </div>
+            </div>
+            <div class="mb-3">
+                <input placeholder="password" name="password" class="form-control">
+                <div class="text-danger">
+                    ${getErrors(errors, 'password')}
+                </div>
+            </div>
+            <div class="mb-3">
+                <input placeholder="confirm password" name="confirmPassword" class="form-control">
+                <div class="text-danger">
+                    ${getErrors(errors, 'confirmPassword')}
+                </div>
+            </div>
+            <button class="btn btn-success" type="submit">Sign Up</button>
+        </form>
+    </div>
+</section>
+ 
 `
 	})
 }
