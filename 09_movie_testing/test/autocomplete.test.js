0b74da956c8d07bec96ee0f835606e6b5ea3e582
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-22 00:59:34 +0630

    Displaying autocomplete

--- 09_movie_testing/test/autocomplete.test.js
+++ 09_movie_testing/test/autocomplete.test.js
@@ -1,3 +1,15 @@
-it('should work', function () {
-
+it('should show autocomplete', function () {
+	createAutoComplete({
+		root: document.querySelector('#target'),
+		fetchData() {
+			return [
+				{Title: "One Piece"},
+				{Title: "Dragon Ball Kid"},
+				{Title: "Super Natural"},
+			]
+		},
+		renderOption(movie) {
+			return `${movie.Title}`
+		}
+	})
 }); 
