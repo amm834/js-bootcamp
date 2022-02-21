c33dba97411bb428d2387f897bd7778ccb098d41
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-21 21:24:33 +0630

    Totaling price

--- 07_ecommerence/views/carts/carts.js
+++ 07_ecommerence/views/carts/carts.js
@@ -3,15 +3,38 @@ const layout = require('../layout')
 const carts = (items) => {
 	return items.map(item => {
 		return `
-		<div>${item.product.title} - ${item.product.price}</div>
+		<tr>
+		<td>${item.product.title}</td>
+		<td>${item.product.price}</td>
+		<td><button class="btn btn-danger">Delete </button> </td>
+		<td>$${item.product.price * item.quantity}</td>
+</tr>
 		`
 	}).join('')
 }
 
 module.exports = ({items}) => {
+
+	const grandTotal = items.reduce((prev, item) => {
+		return prev + item.product.price * item.quantity;
+	},0)
+
 	return layout({
 		content: `
-		<div>${carts(items)}</div>
+		<table class="table">
+		<tr>
+		<th>Title</th>
+		<th>Price</th>
+		<th>Action</th>
+		<th>Total</th>
+</tr>
+${carts(items)}</table>
+<tr>
+<td colspan="3">
+$${grandTotal}
+</td>
+</tr>
 		`
 	})
+
 }
