d784749fadd37c89bad5aa7f713230d399f69298
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-02-23 11:45:28 +0630

    Another small testing lol

--- 010_test_me/sample_web_project/test/app.test.js
+++ 010_test_me/sample_web_project/test/app.test.js
@@ -5,3 +5,15 @@ it('should have a input', async () => {
 	const input = dom.window.document.querySelector('input')
 	assert(input)
 })
+
+it('shows an valid email message', async function () {
+	const dom = await render('index.html')
+
+	const input = dom.window.document.querySelector('input')
+	input.value = 'amm@gmail.com'
+	dom.window.document.querySelector('form')
+		.dispatchEvent(new dom.window.Event('input'))
+
+	const header = dom.window.document.querySelector('input')
+	console.log('Content of header', header.innerHTML)
+});
